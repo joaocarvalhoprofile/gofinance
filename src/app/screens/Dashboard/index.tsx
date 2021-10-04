3dda13a7c1dc6725a5e27c5b98b8dc3d7584afed
Joao Carvalho <joaokvalho@gmail.com>
2021-10-04 11:18:13 -0300

    feat: Add header and userinfo

--- src/app/screens/Dashboard/index.tsx
+++ src/app/screens/Dashboard/index.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
 
-import { Container, Title } from './style'
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon
+} from './styles'
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: 'https://avatars.githubusercontent.com/u/45977540?v=4' }}
+            />
+
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>João</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+
+      </Header>
     </Container>
   )
 }
