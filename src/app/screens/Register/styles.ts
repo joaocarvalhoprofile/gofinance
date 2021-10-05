dbf9fa1b9517f1224652d44f6679f77336fa8fd2
Joao Carvalho <joaokvalho@gmail.com>
2021-10-05 12:22:41 -0300

    feat: add styles header

--- src/app/screens/Register/styles.ts
+++ src/app/screens/Register/styles.ts
@@ -1,5 +1,22 @@
 import styled from 'styled-components/native'
 
-export const Container = styled.View`
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
 
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFValue(113)}px;
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  align-items: center;
+  justify-content: flex-end;
+  padding-bottom: ${RFValue(19)}px;
+`
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};  
+  font-size: ${RFValue(18)}px;
+  color: ${({ theme }) => theme.colors.shape};
 `
