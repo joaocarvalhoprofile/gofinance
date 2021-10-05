1fb8cf4e2c793f608f62e80187e339a01d239a92
Joao Carvalho <joaokvalho@gmail.com>
2021-10-05 10:46:06 -0300

    fix: change TransactionList

--- src/app/screens/Dashboard/index.tsx
+++ src/app/screens/Dashboard/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import { getBottomSpace } from 'react-native-iphone-x-helper'
-
 import {
   Container,
   Header,
@@ -20,11 +18,20 @@ import {
 } from './styles'
 
 import { HighLightCard } from '../../components/HighLightCard'
-import { TransactionCard } from '../../components/TransactionCard'
+import {
+  TransactionCard,
+  TransactionCardProps
+} from '../../components/TransactionCard'
+
+export interface DataListProps extends TransactionCardProps {
+  id: string
+}
 
 export function Dashboard() {
-  const data = [
+  const data: DataListProps[] = [
     {
+      id: "1",
+      type: "positive",
       title: "Consultoria de serviços",
       amount: "R$ 15.000,00",
       category: {
@@ -34,20 +41,24 @@ export function Dashboard() {
       date: "30/09/2021"
     },
     {
+      id: "2",
+      type: "negative",
       title: "Carnes e Verduras",
       amount: "R$ 1.000,00",
       category: {
-        name: 'Supermercdo',
-        icon: 'dollar-sign'
+        name: "Supermercado",
+        icon: "coffee"
       },
       date: "02/10/2021"
     },
     {
+      id: "3",
+      type: "negative",
       title: "Aluguel",
       amount: "R$ 950,00",
       category: {
-        name: 'moradia',
-        icon: 'dollar-sign'
+        name: 'Moradia',
+        icon: 'shopping-bag'
       },
       date: "15/09/2021"
     }
@@ -98,11 +109,8 @@ export function Dashboard() {
 
         <TransactionList
           data={data}
+          keyExtractor={item => item.id}
           renderItem={({ item }) => <TransactionCard data={item} />}
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{
-            paddingBottom: getBottomSpace()
-          }}
         />
 
       </Transactions>
--- src/app/screens/Dashboard/styles.ts
+++ src/app/screens/Dashboard/styles.ts
@@ -1,8 +1,11 @@
 import styled from 'styled-components/native'
+import { FlatList } from 'react-native'
 
 import { Feather } from '@expo/vector-icons'
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
-import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+import { getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper'
+
+import { DataListProps } from '.'
 
 export const Container = styled.View`
   flex: 1;
@@ -80,6 +83,9 @@ export const Title = styled.Text`
 
   margin-bottom: ${RFValue(16)}px;
 `
-export const TransactionList = styled.FlatList`
-
-`
+export const TransactionList = styled(
+  FlatList as new () => FlatList<DataListProps>
+).attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { paddingBottom: getBottomSpace() }
+})``
