3bbfebf6aea37f2dd8712bed7726c3f6432ec5b1
Joao Carvalho <joaokvalho@gmail.com>
2021-10-06 17:34:45 -0300

    feat: add styles in CategorySelect

--- src/app/components/CategorySelect/styles.ts
+++ src/app/components/CategorySelect/styles.ts
@@ -1,9 +1,27 @@
 import styled from 'styled-components/native'
 
+import { RFValue } from 'react-native-responsive-fontsize'
 import { Feather } from '@expo/vector-icons'
 
-export const Container = styled.View``
+export const Container = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.7
+})`
+  background-color: ${({ theme }) => theme.colors.shape};
 
-export const Category = styled.Text``
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 
-export const Icon = styled(Feather)``
+  border-radius: 5px;
+  padding: 18px 16px;
+`
+
+export const Category = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
+`
+
+export const Icon = styled(Feather)`
+  font-size: ${RFValue(20)}px;
+  color: ${({ theme }) => theme.colors.text};
+`
